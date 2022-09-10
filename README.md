36a362719b2614cc5d4ef35a2928ed78cddea7f5
baba-s <shotababa1206@gmail.com>
2022-09-10 11:41:30 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Kogane.IEnumerablePeekDebugLogExtensionMethods
+# Kogane IEnumerable Peek Debug Log Extension Methods
+
+LINQ の中間操作で要素を Debug.Log できる拡張メソッド
+
+## 使用例
+
+```csharp
+using System.Linq;
+using Kogane;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    private void Awake()
+    {
+        var list = new[]
+        {
+            "フシギダネ",
+            "フシギソウ",
+            "フシギバナ",
+        };
+
+        var r1 = list.PeekDebugLog().ToArray();
+        var r2 = list.PeekDebugLog( x => $"名前：{x}" ).ToArray();
+        var r3 = list.PeekDebugLogWarning().ToArray();
+        var r4 = list.PeekDebugLogWarning( x => $"名前：{x}" ).ToArray();
+        var r5 = list.PeekDebugLogError().ToArray();
+        var r6 = list.PeekDebugLogError( x => $"名前：{x}" ).ToArray();
+    }
+}
+```
---
+++ Runtime/IEnumerablePeekDebugLogExtensionMethods.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Kogane
+{
+    // ReSharper disable once InconsistentNaming
+    public static class IEnumerablePeekDebugLogExtensionMethods
+    {
+        public delegate void LogDelegate( object item );
+
+        public static LogDelegate OnLog        { get; set; } = item => Debug.Log( item );
+        public static LogDelegate OnLogWarning { get; set; } = item => Debug.LogWarning( item );
+        public static LogDelegate OnLogError   { get; set; } = item => Debug.LogError( item );
+
+        public static IEnumerable<T> PeekDebugLog<T>( [NotNull] this IEnumerable<T> source )
+        {
+            if ( source == null ) throw new ArgumentNullException( nameof( source ) );
+
+            IEnumerable<T> Iterator()
+            {
+                foreach ( var x in source )
+                {
+                    OnLog?.Invoke( x );
+                    yield return x;
+                }
+            }
+
+            return Iterator();
+        }
+
+        public static IEnumerable<T> PeekDebugLog<T>
+        (
+            [NotNull] this IEnumerable<T>  source,
+            [NotNull]      Func<T, object> selector
+        )
+        {
+            if ( source == null ) throw new ArgumentNullException( nameof( source ) );
+            if ( selector == null ) throw new ArgumentNullException( nameof( selector ) );
+
+            IEnumerable<T> Iterator()
+            {
+                foreach ( var x in source )
+                {
+                    OnLog?.Invoke( selector( x ) );
+                    yield return x;
+                }
+            }
+
+            return Iterator();
+        }
+
+        public static IEnumerable<T> PeekDebugLogWarning<T>( [NotNull] this IEnumerable<T> source )
+        {
+            if ( source == null ) throw new ArgumentNullException( nameof( source ) );
+
+            IEnumerable<T> Iterator()
+            {
+                foreach ( var x in source )
+                {
+                    OnLogWarning?.Invoke( x );
+                    yield return x;
+                }
+            }
+
+            return Iterator();
+        }
+
+        public static IEnumerable<T> PeekDebugLogWarning<T>
+        (
+            [NotNull] this IEnumerable<T>  source,
+            [NotNull]      Func<T, object> selector
+        )
+        {
+            if ( source == null ) throw new ArgumentNullException( nameof( source ) );
+            if ( selector == null ) throw new ArgumentNullException( nameof( selector ) );
+
+            IEnumerable<T> Iterator()
+            {
+                foreach ( var x in source )
+                {
+                    OnLogWarning?.Invoke( selector( x ) );
+                    yield return x;
+                }
+            }
+
+            return Iterator();
+        }
+
+        public static IEnumerable<T> PeekDebugLogError<T>( [NotNull] this IEnumerable<T> source )
+        {
+            if ( source == null ) throw new ArgumentNullException( nameof( source ) );
+
+            IEnumerable<T> Iterator()
+            {
+                foreach ( var x in source )
+                {
+                    OnLogError.Invoke( x );
+                    yield return x;
+                }
+            }
+
+            return Iterator();
+        }
+
+        public static IEnumerable<T> PeekDebugLogError<T>
+        (
+            [NotNull] this IEnumerable<T>  source,
+            [NotNull]      Func<T, object> selector
+        )
+        {
+            if ( source == null ) throw new ArgumentNullException( nameof( source ) );
+            if ( selector == null ) throw new ArgumentNullException( nameof( selector ) );
+
+            IEnumerable<T> Iterator()
+            {
+                foreach ( var x in source )
+                {
+                    OnLogError?.Invoke( selector( x ) );
+                    yield return x;
+                }
+            }
+
+            return Iterator();
+        }
+    }
+}
